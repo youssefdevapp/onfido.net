853b80306ea131ea2a8d29ed0317440712d6c50f
Youssef G <youssefdevapp@gmail.com>
2019-12-30 10:53:11 +0100

    Add sdk token creation

---
+++ Onfido/Entities/SdkToken.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Onfido.Entities
+{
+    public class SdkToken
+    {
+        [JsonProperty("token")]
+        public string Token;
+    }
+}
---
+++ Onfido/Resources/SdkTokens.cs
@@ -0,0 +1,43 @@
+﻿using Onfido.Entities;
+using Onfido.Http;
+
+namespace Onfido.Resources
+{
+    public class SdkTokens : OnfidoResource
+    {
+        private IRequestor _requestor;
+
+        public SdkTokens() : this(new Requestor())
+        {
+        }
+
+        public SdkTokens(IRequestor requestor)
+        {
+            _requestor = requestor;
+        }
+
+        public SdkToken CreateForApplication(string applicantId, string applicationId)
+        {
+            return Create(applicantId, applicationId, null);
+        }
+
+        public SdkToken CreateForWeb(string applicantId, string referrer)
+        {
+            return Create(applicantId, null, referrer);
+        }
+
+        private SdkToken Create(string applicantId, string applicationId, string referrer)
+        {
+            const string path = "sdk_token";
+
+            var payload = SerializeEntity(new
+            {
+                applicant_id = applicantId,
+                application_id = applicationId,
+                referrer
+            });
+
+            return _requestor.Post<SdkToken>(path, payload);
+        }
+    }
+}
